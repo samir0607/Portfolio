12a88b73e14a939974883737b9ede73c19c469bf
samir0607 <samir.gupta987@gmail.com>
2025-01-14 18:20:18 +0530

    small fix

--- MyPortfolio/src/sections/Projects.jsx
+++ MyPortfolio/src/sections/Projects.jsx
@@ -28,7 +28,7 @@ const Projects = () => {
 
   const currentProject = myProjects[selectedProjectIndex];
   return (
-    <section className='c-space my-20'>
+    <section className='c-space my-20' id='work'>
       <p className='head-text'>Work/Awards</p>
       
       <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
