46740dad6ac060bdf237d46a2f63e33ad8b767c6
samir0607 <samir.gupta987@gmail.com>
2025-06-08 00:54:07 +0530

    fix: adjust astronaut position calculations

--- MyPortfolio/src/constants/index.js
+++ MyPortfolio/src/constants/index.js
@@ -174,7 +174,7 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-    astronautPosition: isSmall ? [5, -5, 0] : isMobile ? [6, -5, 0] : isTablet ? [6, -5, 0] : [11, -6, 0],
+    astronautPosition: isSmall ? [4, -5, 0] : isMobile ? [4.5, -5, 0] : isTablet ? [5, -5, 0] : [11, -6, 0],
     reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
     ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
     targetPosition: isSmall ? [-5, 0, -10] : isMobile ? [-9, 0, -10] : isTablet ? [-11, 0, -10] : [-13, 0, -10],
