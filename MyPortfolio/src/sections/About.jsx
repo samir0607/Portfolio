e6549c59e4f6686057d95fdeb9c33c3693793862
samir0607 <samir.gupta987@gmail.com>
2025-10-02 02:34:08 +0530

    small change

--- MyPortfolio/src/sections/About.jsx
+++ MyPortfolio/src/sections/About.jsx
@@ -41,10 +41,7 @@ const About = () => {
               <Globe
                 height={326}
                 width={326}
-                backgroundColor="rgba(0,0,0,0)"
-                backgroundImageOpacity={0.5}
                 showAtmosphere
-                showGraticules
                 globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
                 backgroundImageUrl='//unpkg.com/three-globe/example/img/night-sky.png'
